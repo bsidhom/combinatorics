0d6abe170dc1053912cb03e4d79e6317904e646b
Ben Sidhom <bsidhom@gmail.com>
2025-01-24 22:13:57 -0800

    Play around with compositions and partitions
    Stars and bars seems to corroborate the results

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -8,17 +8,29 @@ let partitions_up_to_part_size n =
   in
   prod 1 Series.one
 
+let _ = partitions_up_to_part_size
+
 let () =
+  (*
+  let nickels = Series.((one - zpow 30) / (one - zpow 5)) in
+  let dimes = Series.((one - zpow 40) / (one - zpow 10)) in
+  let quarters = Series.((one - zpow 75) / (one - zpow 25)) in
+  let fs = Series.(nickels * dimes * quarters) in
+  *)
+  (*
   let pennies = Series.(one / (one - z)) in
   let nickels = Series.(one / (one - zpow 5)) in
   let dimes = Series.(one / (one - zpow 10)) in
   let quarters = Series.(one / (one - zpow 25)) in
   let fs = Series.(pennies * nickels * dimes * quarters) in
-  let _ = fs in
+  *)
   let fs = partitions_up_to_part_size 100 in
+  let _ = fs in
+  let fs = Series.(one / (one - z)) in
+  let fs = Series.(fs * fs * fs * fs * fs) in
   let print (i, c) =
     let s = Q.to_string c in
-    Printf.printf "%d: %s\n%!" i s
+    Printf.printf "%03d: %s\n%!" i s
   in
   let fs = Seq.zip (Seq.ints 0) fs in
   Seq.take 101 fs |> Seq.iter print
